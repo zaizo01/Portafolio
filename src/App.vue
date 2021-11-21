16d4aefe96f3cf47bdcbd7e475d4696fc926f5c3
zaizo01 <e.zaizortega@gamil.com>
2021-11-20 23:34:16 -0400

    Changes information in my portafolio

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="app">
     <div id="nav">
-     <h2>Cristopher Zaiz Ortega</h2>
+      <div>
+        <img src="./assets/building.png" alt="building image" width="500px" height="500px">
+      </div>
+      <vue-typer :text='description' erase-style='backspace'></vue-typer>
     </div>
   </div>
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      description: `Hi everyone, I'm Cristopher Zaiz and I am building my portfolio.`
+    }
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueTyperPlugin from 'vue-typer'
+
+Vue.use(VueTyperPlugin)
 
 Vue.config.productionTip = false
 
